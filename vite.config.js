14ae5cbde6ab4a9391d3d71f356c840efdb66053
pablo <pablo_lo_mas_k@hotmail.com>
2026-01-12 06:02:14 +0100

    extensiones arreglo

--- vite.config.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: '/pamelanutricion/' // nombre del repo hosteado en github pages queda web.etc"/pamelanutricion"  - si tuvieramos un host propio hay que eliminar esto porque la base seria "/"
-})
